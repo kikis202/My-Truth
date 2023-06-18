50afc50a5a71074e3c464cfed7592c5ee45b5c41
Kristaps <kristaps1000@gmail.com>
2023-06-19 00:09:22 +0300

    New files for routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { LoadingPage } from "~/components/loading";
 import { useState } from "react";
 import toast, { LoaderIcon } from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -87,22 +88,28 @@ const PostView = (props: PostWithUser) => {
       className="flex gap-4 border-b border-slate-400 bg-gray-800 p-8"
     >
       <div>
-        <Image
-          src={author.profileImageUrl}
-          alt="Profile image"
-          className="h-10 w-10 rounded-full"
-          width={40}
-          height={40}
-        />
+        <Link href={author.id}>
+          <Image
+            src={author.profileImageUrl}
+            alt="Profile image"
+            className="h-10 w-10 rounded-full"
+            width={40}
+            height={40}
+          />
+        </Link>
       </div>
       <div className="flex flex-col">
         <div className="flex items-center gap-1">
-          <span className="text-sm text-slate-400">{`@${author.username}`}</span>
+          <Link href={author.id}>
+            <span className="text-sm text-slate-400">{`@${author.username}`}</span>
+          </Link>
           <span className="text-xs font-thin text-slate-500">
             {`- ${dayjs(post.createdAt).fromNow()}`}
           </span>
         </div>
-        <span className="text-lg text-slate-300">{post.content}</span>
+        <Link href={`/post/${post.id}`}>
+          <span className="text-lg text-slate-300">{post.content}</span>
+        </Link>
       </div>
     </div>
   );
